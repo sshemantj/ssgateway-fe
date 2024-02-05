8e3f0d1aa921a8eb4272b2c35c88dc11d2e12728
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-02-05 12:05:55 +0530

    folder structure updated

---
+++ src/services/client.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { API_BASE_URL } from "../constants/allEnv";
+
+export default axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
+
+export const axiosPublic = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
