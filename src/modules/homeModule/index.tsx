b59b2623074048c5e9e95ec6860cd3e217547d68
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-02-06 10:00:38 +0530

    home screen started

--- src/modules/homeModule/index.tsx
+++ src/modules/homeModule/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Button } from "@mui/material";
+import styles from "./homemodule.module.scss";
 
 const HomeModule = () => {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className={styles.homeWrapper}>
+      <div className={styles.headingWrapper}>
+        <p>Welcome to</p>
+        <p className={styles.scanAndGo}>
+          Scan <br /> & <br /> Go
+        </p>
+      </div>
+      <h4>Max 5 products</h4>
+      <h4>Max 10,000 Rs</h4>
+      <div className={styles.scanningBtnContainer}>
+        <Button variant="outlined">Start scanning</Button>
+      </div>
     </div>
   );
 };
