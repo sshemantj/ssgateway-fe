12dcd7d681315690a1224120171d486606cbbae6
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-02-06 23:49:10 +0530

    qr code screen complete

--- src/modules/process/showQrScreen/index.tsx
+++ src/modules/process/showQrScreen/index.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import QRCode from "qrcode.react";
 import styles from "./showQrScreen.module.scss";
 
 const ShowQrScreen = () => {
-  return <div>ShowQrScreen</div>;
+  const [inputValue, setInputValue] = useState("this is demo 323232");
+
+  return (
+    <div className={styles.showQrScreenWrapper}>
+      <QRCode value={inputValue} />
+      <div className={styles.instruction}>
+        <h4>Show this QR code to the associate and you are done.</h4>
+      </div>
+    </div>
+  );
 };
 
 export default ShowQrScreen;
