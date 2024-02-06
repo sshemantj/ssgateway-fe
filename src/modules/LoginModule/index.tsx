194e01d28645bed46576bc74013534187fcbc729
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-02-06 10:35:52 +0530

    login page done

--- src/modules/LoginModule/index.tsx
+++ src/modules/LoginModule/index.tsx
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import styles from "./login.module.scss";
+import {
+  Checkbox,
+  Grid,
+  TextField,
+  FormControlLabel,
+  Paper,
+  Button,
+} from "@mui/material";
 
 const LoginModule = () => {
+  const [checked, setChecked] = useState(true);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className={styles.loginWrapper}>
+      <div style={{ padding: 30 }}>
+        <Paper>
+          <Grid
+            container
+            // spacing={3}
+            direction={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Grid item xs={12} margin={"0 0 1rem 0"}>
+              <TextField label="Username"></TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField label="Password" type={"password"}></TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checked}
+                    onChange={handleChange}
+                    inputProps={{ "aria-label": "primary checkbox" }}
+                  />
+                }
+                label="Keep me logged in"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                sx={{ width: "100%", margin: "1rem 0" }}
+                variant="contained"
+              >
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
     </div>
   );
 };
