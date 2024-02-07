c060fe1be7d3a1d9a9f2e2377f71c6e8b1813839
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-02-07 16:50:08 +0530

    changes

---
+++ src/component/molecules/customBarcodeScanner2/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./CustomBarcodeScanner2.module.scss";
+
+const CustomBarcodeScanner2 = () => {
+  return (
+    <div className={styles.customBarcodeWrapper}>CustomBarcodeScanner2</div>
+  );
+};
+
+export default CustomBarcodeScanner2;
