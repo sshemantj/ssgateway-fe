d5b406e496fef35e457972cdf8b056714b03953c
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-02-08 12:31:36 +0530

    changes

--- src/modules/homeModule/index.tsx
+++ src/modules/homeModule/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import WelcomeScreen from "@/component/atoms/welcomeScreen";
 import styles from "./homemodule.module.scss";
+import CustomBarcodeScanner from "@/component/molecules/customBarcodeScanner";
 
 const HomeModule = () => {
   return (
     <div className={styles.homeWrapper}>
-      <WelcomeScreen />
+      <div className={styles.headingWrapper}>
+        <p>
+          Scan store QR-code <br /> to continue
+        </p>
+        <CustomBarcodeScanner
+          fps={10}
+          qrbox={250}
+          disableFlip={false}
+          qrCodeSuccessCallback={(result) => console.log(result)}
+          qrCodeErrorCallback={(error) => console.log(error)}
+          showZoomSliderIfSupported={true}
+        />
+      </div>
     </div>
   );
 };
