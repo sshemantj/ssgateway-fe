0c39c8bba74003b6a62958e736c963370c1e6384
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-02-09 10:44:20 +0530

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { Providers } from "@/store/provider";
 import "@/styles/globals.css";
 import "@/styles/barCodeStyle.scss";
 // import "@/styles/qrCodeStyle.scss";
-import "../styles/";
 import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
 
