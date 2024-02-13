a845a7e066249eb032fc3128841ef07e271bf47e
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-02-13 10:48:42 +0530

    changes

---
+++ src/pages/demo.tsx
@@ -0,0 +1,42 @@
+import { NextPage } from "next";
+import Navbar from "@/component/molecules/Navbar";
+import Head from "next/head";
+import CustomQrcodeScanner from "@/component/molecules/customQrcodeScanner";
+import { ForwardedRef, Ref, useRef } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+
+const Home: NextPage = () => {
+  const ref = useRef<Html5QrcodeScanner | null>(null);
+
+  return (
+    <>
+      <Head>
+        <title>Scan-&-go!</title>
+      </Head>
+      <Navbar />
+      <main
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          //   display: "flex",
+          //   alignItems: "center",
+          //   justifyContent: "center",
+        }}
+      >
+        <CustomQrcodeScanner
+          ref={ref as Ref<ForwardedRef<Html5QrcodeScanner | null>>}
+          fps={10}
+          qrbox={250}
+          disableFlip={false}
+          defaultZoomValueIfSupported={4}
+          qrCodeSuccessCallback={() => {}}
+          qrCodeErrorCallback={(error) => console.log(error)}
+          showZoomSliderIfSupported={true}
+        />
+      </main>
+    </>
+  );
+};
+
+export default Home;
