15023f1f2e88f9d3be1cb58d891ba4e35834ac2e
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-04-16 18:08:28 +0530

    card variant added

--- src/modules/demoModule/index.tsx
+++ src/modules/demoModule/index.tsx
@@ -1,10 +1,13 @@
-import Cards from "@/component/atoms/cards";
 import React from "react";
+import Cards from "@/component/atoms/cards";
+import styles from "./demoModule.module.scss";
 
 const DemoModule = () => {
   return (
-    <div>
-      <Cards />
+    <div className={styles.demo_wrapper}>
+      <Cards variant="primary" />
+      <Cards variant="success" />
+      <Cards variant="warning" />
     </div>
   );
 };
