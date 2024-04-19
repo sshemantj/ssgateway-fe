797947063d8cc28da9770ab112a3ca6b359c0500
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-04-19 10:54:31 +0530

    navlist

--- src/modules/newPanelModule/index.tsx
+++ src/modules/newPanelModule/index.tsx
@@ -4,8 +4,8 @@ import HomeModule from "../homeModule";
 
 const NewPanelModule = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid sm={12} md={12} item>
+    <Grid container spacing={2} width="100%">
+      <Grid sm={12} md={12} item width="100%">
         <HomeModule />
       </Grid>
     </Grid>
