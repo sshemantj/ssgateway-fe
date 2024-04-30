1a7f714c99c4cde87656847333797e0d83374086
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-04-30 10:38:21 +0530

    counter reducer removed

--- src/store/combineReducers.ts
+++ src/store/combineReducers.ts
@@ -1,5 +1,4 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import counterReducer from "./slices/counterSlice";
 import gatewaySlice from "./slices/gatewaySlice";
 import menu from "./slices/menu";
 import login from "./slices/loginSlice";
@@ -8,7 +7,6 @@ const rootReducer = combineReducers({
   menu,
   login,
   gateway: gatewaySlice,
-  counter: counterReducer,
 });
 
 export { rootReducer };
