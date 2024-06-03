b24d4c59a500ee56004adeea9542aa83861aa7a6
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-06-03 10:21:32 +0530

    unapproved pagination complete

--- src/modules/dashboardModule/unApprovedModule/index.tsx
+++ src/modules/dashboardModule/unApprovedModule/index.tsx
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from "react";
 import { unApprovedColumns, unApprovedRows } from "@/constants/tableConstant";
 import FeaturedTable from "@/tables/featuredTable";
 import { Box } from "@mui/material";
-import { GridRowSelectionModel } from "@mui/x-data-grid";
+import {
+  GridCallbackDetails,
+  GridPaginationModel,
+  GridRowSelectionModel,
+} from "@mui/x-data-grid";
 import UnApprovedFooter from "./unApprovedFooter";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { approveSizevariants } from "@/services/thunks/tableApis";
+import {
+  approveSizevariants,
+  fetchTableData,
+} from "@/services/thunks/tableApis";
 import toast, { Toaster } from "react-hot-toast";
 import useTableData from "@/hooks/useTableData";
+import { IProductsTypes } from "@/interfaces/product";
 
 interface IProps {}
 
@@ -17,11 +25,16 @@ const UnapprovedModule = (props: IProps) => {
   const unAprovedProducts = useAppSelector(
     (state) => state.gateway.unAprovedProducts
   );
+  const { selectedChannel } = useAppSelector((state) => state.gateway);
   const dispatch = useAppDispatch();
   const getTableData = useTableData();
+
   const [tableState, setTableState] = useState({
     columns: unApprovedColumns,
     rows: [],
+    isLoading: false,
+    page: 1,
+    pageSize: 10,
   });
 
   useEffect(() => {
@@ -45,6 +58,23 @@ const UnapprovedModule = (props: IProps) => {
       });
     }
   }, [unAprovedProducts]);
+
+  useEffect(() => {
+    setTableState((old) => ({ ...old, isLoading: true }));
+    dispatch(
+      fetchTableData({
+        channelid: selectedChannel,
+        pageNumber: tableState.page,
+        pageSize: tableState.pageSize,
+        type: IProductsTypes.UNAPPROVED,
+      })
+    ).then(() => {
+      setTableState((old) => ({
+        ...old,
+        isLoading: false,
+      }));
+    });
+  }, [tableState.page, tableState.pageSize]);
 
   const [selectedTableRows, setSelectedTableRows] =
     useState<GridRowSelectionModel>([]);
@@ -75,22 +105,35 @@ const UnapprovedModule = (props: IProps) => {
   return (
     <Box width="100%">
       <FeaturedTable
-        slots={{
-          footer: () => (
-            <UnApprovedFooter
-              {...{
-                handleApprovedProduct,
-                isDisabled: !!!selectedTableRows?.length,
-              }}
-            />
-          ),
-        }}
         {...{
+          loading: tableState.isLoading,
           rows: tableState.rows,
           columns: tableState.columns,
           checkboxSelection: true,
           onRowSelectionModelChange,
           rowCount: unAprovedProducts?.totalRecords,
+          paginationMode: "server",
+          // paginationModel: {
+          //   page: tableState.page - 1,
+          //   pageSize: tableState.pageSize,
+          // },
+          onPaginationModelChange: ({
+            page,
+            pageSize,
+          }: GridPaginationModel) => {
+            console.log({ page, pageSize });
+            setTableState((old) => ({ ...old, page, pageSize }));
+          },
+          slots: {
+            footer: () => (
+              <UnApprovedFooter
+                {...{
+                  handleApprovedProduct,
+                  isDisabled: !!!selectedTableRows?.length,
+                }}
+              />
+            ),
+          },
         }}
       />
       <Toaster />
