6fbbce7de2743e6f79f826d7c42547f61c908942
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-06-18 16:01:22 +0530

    navbar admin options commented

--- src/constants/navlistArr.tsx
+++ src/constants/navlistArr.tsx
@@ -67,75 +67,75 @@ export const navListArr: INavListArr[] = [
       },
     ],
   },
-  {
-    topHeading: "User profile",
-    subHeaderList: [
-      {
-        title: "Update password",
-        icon: "",
-        path: IAllRoutes.USER_PROFILE,
-        value: IProfileSubRoutes.UPDATE_PASSWORD,
-        iconJsx: <FolderCopyIcon color="inherit" />,
-      },
-      {
-        title: "Update Profile",
-        icon: "",
-        path: IAllRoutes.USER_PROFILE,
-        value: IProfileSubRoutes.UPDATE_PROFILE,
-        iconJsx: <ServiceIcon color="inherit" />,
-      },
-    ],
-  },
-  {
-    topHeading: "Manage Channels",
-    subHeaderList: [
-      {
-        title: "Add",
-        icon: "",
-        iconJsx: <OfflineBoltIcon color="inherit" />,
-        path: IAllRoutes.MANAGE_CHANNELS,
-        value: IChannelSubRoutes.ADD_CHANNEL,
-      },
-      {
-        title: "Update",
-        icon: "",
-        iconJsx: <QueueIcon color="inherit" />,
-        path: IAllRoutes.MANAGE_CHANNELS,
-        value: IChannelSubRoutes.UPDATE_CHANNEL,
-      },
-      {
-        title: "View All Channels",
-        icon: "",
-        iconJsx: <WalletIcon color="inherit" />,
-        path: IAllRoutes.MANAGE_CHANNELS,
-        value: IChannelSubRoutes.VIEW_ALL_CHANNEL,
-      },
-    ],
-  },
-  {
-    topHeading: "Channel Mapping",
-    subHeaderList: [
-      // {
-      //   title: "View User channel",
-      //   icon: "",
-      //   iconJsx: <OfflineBoltIcon color="inherit" />,
-      //   path: IAllRoutes.CHANNEL_MAPPINGS,
-      //   value: IChannelMappingSubRoutes.VIEW_CHANNEL,
-      // },
-      {
-        title: "Map User with channels",
-        icon: "",
-        iconJsx: <QueueIcon color="inherit" />,
-        path: IAllRoutes.CHANNEL_MAPPINGS,
-        value: IChannelMappingSubRoutes.MAP_CHANNEL,
-      },
-      {
-        title: "Bulk Channel Mappings",
-        icon: "",
-        iconJsx: <QueueIcon color="inherit" />,
-        path: IAllRoutes.CHANNEL_MAPPINGS,
-        value: IChannelMappingSubRoutes.BULK_UPLOAD,
-      },
-    ],
-  },
+  // {
+  //   topHeading: "User profile",
+  //   subHeaderList: [
+  //     {
+  //       title: "Update password",
+  //       icon: "",
+  //       path: IAllRoutes.USER_PROFILE,
+  //       value: IProfileSubRoutes.UPDATE_PASSWORD,
+  //       iconJsx: <FolderCopyIcon color="inherit" />,
+  //     },
+  //     {
+  //       title: "Update Profile",
+  //       icon: "",
+  //       path: IAllRoutes.USER_PROFILE,
+  //       value: IProfileSubRoutes.UPDATE_PROFILE,
+  //       iconJsx: <ServiceIcon color="inherit" />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   topHeading: "Manage Channels",
+  //   subHeaderList: [
+  //     {
+  //       title: "Add",
+  //       icon: "",
+  //       iconJsx: <OfflineBoltIcon color="inherit" />,
+  //       path: IAllRoutes.MANAGE_CHANNELS,
+  //       value: IChannelSubRoutes.ADD_CHANNEL,
+  //     },
+  //     {
+  //       title: "Update",
+  //       icon: "",
+  //       iconJsx: <QueueIcon color="inherit" />,
+  //       path: IAllRoutes.MANAGE_CHANNELS,
+  //       value: IChannelSubRoutes.UPDATE_CHANNEL,
+  //     },
+  //     {
+  //       title: "View All Channels",
+  //       icon: "",
+  //       iconJsx: <WalletIcon color="inherit" />,
+  //       path: IAllRoutes.MANAGE_CHANNELS,
+  //       value: IChannelSubRoutes.VIEW_ALL_CHANNEL,
+  //     },
+  //   ],
+  // },
+  // {
+  //   topHeading: "Channel Mapping",
+  //   subHeaderList: [
+  //     // {
+  //     //   title: "View User channel",
+  //     //   icon: "",
+  //     //   iconJsx: <OfflineBoltIcon color="inherit" />,
+  //     //   path: IAllRoutes.CHANNEL_MAPPINGS,
+  //     //   value: IChannelMappingSubRoutes.VIEW_CHANNEL,
+  //     // },
+  //     {
+  //       title: "Map User with channels",
+  //       icon: "",
+  //       iconJsx: <QueueIcon color="inherit" />,
+  //       path: IAllRoutes.CHANNEL_MAPPINGS,
+  //       value: IChannelMappingSubRoutes.MAP_CHANNEL,
+  //     },
+  //     {
+  //       title: "Bulk Channel Mappings",
+  //       icon: "",
+  //       iconJsx: <QueueIcon color="inherit" />,
+  //       path: IAllRoutes.CHANNEL_MAPPINGS,
+  //       value: IChannelMappingSubRoutes.BULK_UPLOAD,
+  //     },
+  //   ],
+  // },
 ];
