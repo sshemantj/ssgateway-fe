e7d841ec1ccec2e7c86985b2255cfcfdafdf6c7d
Muralidevan <murali@theretailinsights.com>
2024-07-11 20:55:03 +0530

    fix: tslint error

--- src/modules/userProfileModule/index.tsx
+++ src/modules/userProfileModule/index.tsx
@@ -1,9 +1,5 @@
-import React from "react";
 import { useSearchParams } from "next/navigation";
 import styles from "./userProfile.module.scss";
-import { IProfileSubRoutes } from "@/constants/allRoutes";
-import UpdatePassword from "./updatePassword";
-import UpdateProfile from "./updateProfile";
 
 const UserProfileModule = () => {
   const searchParams = useSearchParams();
@@ -11,8 +7,9 @@ const UserProfileModule = () => {
 
   return (
     <div className={styles.profileWrapper}>
-      {screen === IProfileSubRoutes.UPDATE_PASSWORD ? <UpdatePassword /> : null}
-      {screen === IProfileSubRoutes.UPDATE_PROFILE ? <UpdateProfile /> : null}
+      <p>user profile old</p>
+      {/* {screen === IProfileSubRoutes.UPDATE_PASSWORD ? <UpdatePassword /> : null}
+      {screen === IProfileSubRoutes.UPDATE_PROFILE ? <UpdateProfile /> : null} */}
     </div>
   );
 };
